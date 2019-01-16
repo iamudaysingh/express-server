48633de69f1a0a0a78d25fed2e9807c8a3e9e455
udaysingh <uday.singh@successive.tech>
2019-01-16 16:05:56 +0530

    docs: created Diamond_pattern.js & Equilateral_pattern.js

---
+++ extra/patterns/Diamond_pattern.js
@@ -0,0 +1,36 @@
+function triangle(n)
+{
+    let str = '';
+    for(let i=1; i<=n; i++)
+    {
+        for(let k=1; k<=n-i; k++)
+        {
+            str += " ";
+        }
+        for(let j=1; j<=i; j++)
+        {
+            str += "* ";
+
+        }
+        console.log(str);
+        str = "";
+       console.log('');
+    }
+    let str1 = '';
+    for(let i=1; i<=n; i++)
+    {
+        for(let k=1; k<i; k++)
+        {
+            str1 += " ";
+        }
+        for(let j=i; j<=n; j++)
+        {
+            str1 += "* ";
+
+        }
+        console.log(str1);
+        str1 = "";
+       console.log('');
+    }
+}
+triangle(5);
