22e39b170b531bdc0e9aa5147daed3cff2c15136
udaysingh <uday.singh@successive.tech>
2019-01-18 12:34:58 +0530

    docs: Create validation.js and hasPermission.js

--- extra/utils/hasPermission.js
+++ extra/utils/hasPermission.js
@@ -1,46 +1,21 @@
+const getUsers = "getUsers";
+const head_trainer = "head_trainer";
+const trainer = "trainer";
+const trainer1 = "trainee";
 let permissions = {
-'getUsers': {
-all: ['head-trainer'],
-read : ['trainee', 'trainer'],
-write : ['trainer'],
-delete: [],
+  getUsers: {
+    all: ["head-trainer"],
+    read: ["trainee", "trainer"],
+    write: ["trainer"],
+    delete: []
+  }
+};
+function hasPermission(module_name, role, permissionType) {
+  //let {getUsers : { all : [ x ] , read : [ b , c ] , write : [ d ] , delete : [] } } = permissions;
+  if (module_name == "getUsers") {
+    if (permissions[module_name][permissionType].includes(role)|| permissionType== "all") {
+      console.log("true");
+    } else console.log("false");
+  }
 }
-}
-function hasPermission ( getUsers , role , permissionType )
-{
-let {getUsers : { all : [ x ] , read : [ b , c ] , write : [ d ] , delete : [] } } = permissions;
-if( role == x)
-{
-if( permissionType == "all" || permissionType == "write" || permissionType == "read" || permissionType == "delete")
-{
-return (console.log("True"));
-}
-else
-{
-return (console.log("False"));
-}
-}
-if( role == b)
-{
-if(permissionType == "read" )
-{
-return (console.log("True"));
-}
-else
-{
-return (console.log("False"));
-}
-}
-if( role == c)
-{
-if(permissionType == "read" || permissionType == "write")
-{
-return (console.log("True"));
-}
-else
-{
-return (console.log("False"));
-}
-}
-}
-hasPermission ("getUsers" , "trainee" , "write" );
+hasPermission("getUsers", "trainer", "write");
--- extra/utils/validation.js
+++ extra/utils/validation.js
@@ -1,18 +1,18 @@
 const user = [
   {
-  traineeEmail: 'trainee1@successive.tech',
-  reviewerEmail: 'reviewer1@successive.tech',
-  meraEmail: 'tgxcsdu@cdbhs.com'
+    traineeEmail: "trainee1@successive.tech",
+    reviewerEmail: "reviewer1@successive.tech",
+    meraEmail: "tgxcsdu@cdbhs.com"
   },
   {
-    traineeEmail1: 'trainee1@successive.tech',
-    reviewerEmail2: 'reviewer1@successive.tech',
-    meraEmail3: 'tgxcsdu@cdbhs.com'
+    traineeEmail1: "trainee1@successive.tech",
+    reviewerEmail2: "reviewer1@successive.tech",
+    meraEmail3: "tgxcsdu@cdbhs.com"
   }
 ];
 function validateEmail(email) {
-  let regex = /^[A-Za-z0-9._%+-]+@successive.tech$/
-  return (regex.test(email));
+  let regex = /^[A-Za-z0-9._%+-]+@successive.tech$/;
+  return regex.test(email);
 }
 /*function validateUsers(user) {
   user.forEach(function (us) {
@@ -25,14 +25,13 @@ validateUsers(user); */
    "trainee1@successive.tech","reviewer1@successive.tech","123456"
 
 ];*/
-function ValidateUser(user1)
-{
-//let regex = /^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+\.)?[a-zA-Z]+\.)?(successive)\.tech$/
-//var values = [];
-/*values = Object.values(user);
+function ValidateUser(user1) {
+  //let regex = /^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+\.)?[a-zA-Z]+\.)?(successive)\.tech$/
+  //var values = [];
+  /*values = Object.values(user);
 for(let i=0;i<=2;i++) {
   console.log(values[i]);*/
-/*if((regex.test(values[i])))
+  /*if((regex.test(values[i])))
 {
   console.log(true);
 }
@@ -40,7 +39,7 @@ else
 {
   console.log(false);
 }*/
-/*let v=0,inv=0;
+  /*let v=0,inv=0;
 let v1 =[];
 let in1=[];
 user1.forEach(function(user) {
@@ -59,41 +58,39 @@ user1.forEach(function(user) {
 });
 console.log(v);
 console.log(v1);*/
-let v1=[],count=0;
-user.forEach((element1,index)=> {
-Object.keys(element1).forEach((element2,index)=>{
-//console.log(element1[element2]);
-v1[count]=element1[element2];
-count++;
-});
-});
-console.log("INPUT IS AS FOLLOWS");
-console.log(v1);
-let valid =[];
-let invalid=[];
-let vc=0;
-let ic=0;
-for(let i=0;i<count;i++) {
-let regex = /^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+\.)?[a-zA-Z]+\.)?(successive)\.tech$/
-if((regex.test(v1[i])))
-{
-  //console.log(V1[i]);
-  valid[vc]=v1[i];
-  vc++;
-  //console.log(true);
-}
-else
-{
-  //console.log(V1[i]);
-  invalid[ic]=v1[i];
-  ic++;
-  //console.log(false);
-}
-}
+  let v1 = [],
+    count = 0;
+  user.forEach((element1, index) => {
+    Object.keys(element1).forEach((element2, index) => {
+      //console.log(element1[element2]);
+      v1[count] = element1[element2];
+      count++;
+    });
+  });
+  console.log("INPUT IS AS FOLLOWS");
+  console.log(v1);
+  let valid = [];
+  let invalid = [];
+  let vc = 0;
+  let ic = 0;
+  for (let i = 0; i < count; i++) {
+    let regex = /^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+\.)?[a-zA-Z]+\.)?(successive)\.tech$/;
+    if (regex.test(v1[i])) {
+      //console.log(V1[i]);
+      valid[vc] = v1[i];
+      vc++;
+      //console.log(true);
+    } else {
+      //console.log(V1[i]);
+      invalid[ic] = v1[i];
+      ic++;
+      //console.log(false);
+    }
+  }
 
-console.log('no of valid users' , vc);
-console.log('valid users are as follows' , valid);
-console.log('no of invalid users',ic);
-console.log('invalid users are as follows',invalid);
+  console.log("no of valid users", vc);
+  console.log("valid users are as follows", valid);
+  console.log("no of invalid users", ic);
+  console.log("invalid users are as follows", invalid);
 }
 ValidateUser(user);
