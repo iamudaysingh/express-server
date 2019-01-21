268b7d17c379b83c8b56dcfdff9af658b6b7a5aa
udaysingh <uday.singh@successive.tech>
2019-01-21 18:36:20 +0530

    docs: conversion of .js file into .ts file and implementation of interface

--- extraTs/utils/helper.ts
+++ extraTs/utils/helper.ts
@@ -1,4 +1,4 @@
-export function validateEmail(email) {
+export function validateEmail(email : string) : boolean {
   let regex = /^[A-Za-z0-9._%+-]+@successive.tech$/;
   return regex.test(email);
 }
