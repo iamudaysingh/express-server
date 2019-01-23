c121e4e9cb9144b7fd4a549b59250c1d6293d7aa
udaysingh <uday.singh@successive.tech>
2019-01-23 17:49:25 +0530

    :feat: Implementation of Middleware , Created errorHandler.ts,index.ts and notFoundRoute.ts , Sent response in .json format

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,29 @@
 import * as express from "express";
+import * as bodyParser from "body-parser";
+import { notFoundRoutes, errorHandler } from "./libs/routes";
 export class Server {
   private app: express.Express;
   constructor(private config) {
     this.app = express();
   }
+  public initBodyParser() {
+    const { app } = this;
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+  }
   public bootstrap() {
+    this.initBodyParser();
     this.setupRoutes();
     return this;
   }
   public setupRoutes() {
     const { app } = this;
-    app.use("/health-check", (req, res) => {
+    app.use("/health-check", (req, res, next) => {
       console.log("INSIDE SETUPROUTES");
       res.send("I AM OK");
     });
+    app.use(notFoundRoutes);
+    app.use(errorHandler);
   }
   public run() {
     const {
